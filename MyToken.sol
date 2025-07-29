840d9a16b1a82e7712ea05ec5d53a647abaa3928
Abderrahmane <abderrahmaneyoub94@gmail.com>
2025-07-29 08:37:01 +0100

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+
+    constructor(uint256 initialSupply) ERC20("CryptoDevToken", "CDT") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount * 10 ** decimals());
+    }
+}
